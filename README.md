fb1701c78f486d83dacd9bf2bd397e81fc23205d
KentTho <havantho2004@gmail.com>
2025-09-05 23:35:27 +0700

    Folder at structure added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # End_to_end_Medical_Chatbot
+
+
+# 04/09 2025
+# ~~~~~~~~~~~ Day 3 ~~~~~~~~~~
+# template.py => Viết đàu tiên
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+from setuptools.config.expand import find_packages
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author= 'Kent Ha',
+    author_email= 'havantho2004@gmail.com',
+    packages = find_packages(),
+    install_requires =[]
+)
